d061b6755916729d6c4e1f7f03a5eb83cd4ee631
DinobergueViana <dinobergueviana@gmail.com>
2021-01-14 16:44:06 -0300

    adicionando botoes ao banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,9 +1,17 @@
 import './Banner.css'
+import arrowLeft from '../../assets/arrow-left.svg'
+import arrowRight from '../../assets/arrow-right.svg'
 
 const Banner = () => {
   return (
     <section className="banner">
-      <img src="https://images.homify.com/c_fill,f_auto,h_500,q_auto,w_1280/v1508253587/p/photo/image/2275988/rf_interiores-7861.jpg" alt="Banner"/>
+      <div className="arrow">
+        <img src={arrowLeft} alt=""/> 
+      </div>
+      {/* <img src="https://images.homify.com/c_fill,f_auto,h_500,q_auto,w_1280/v1508253587/p/photo/image/2275988/rf_interiores-7861.jpg" alt="Banner"/> */}
+      <div className="arrow">
+        <img src={arrowRight} alt=""/>
+      </div> 
     </section>
   )
 }
